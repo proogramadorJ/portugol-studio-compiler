586dffa510f58a4bb5414f5d2a7e29414c58663a
Pedro <pedroenxadrista1@gmail.com>
2026-05-07 23:31:41 -0300

    Implementação de operadores lógicos com curto-circuito na VM e no gerador de bytecode. Foram adicionados os opcodes `POP`, `DUP` e `JMP_IF_TRUE`, além da lógica para tratar as expressões `e` e `ou` no `ByteCodeGenerator`.

---
+++ vm/src/commonMain/kotlin/io/PortugolConsole.kt
@@ -0,0 +1,7 @@
+package io
+
+interface PortugolConsole {
+    fun print(message: String)
+    fun println(message : String) // TODO confirmar se tem escreval nesse dialeto portugol
+    fun clear()
+}
